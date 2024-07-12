513710028c311107ef3997b9e77d4dbb23e9d5ec
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-07-12 02:00:50 +0000

    added list of messages

--- chat.rb
+++ chat.rb
@@ -4,20 +4,22 @@ pp "howdy"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
-
+list_of_messages = [
+  { :role => "system", :content => "You are a helpful assistant."},
+  { :role => "user", :content => "What are the best pizza spots in Chicago?"}
+]
 
 raw_response = client.chat(
   parameters: {
     model: "gpt-3.5-turbo",
-    messages: [
-      { :role => "system", :content => "You are a helpful assistant."},
-      { :role => "user", :content => "What are the best pizza spots in Chicago?"}
-    ]
+    messages: list_of_messages
   }
 )
 
 pp raw_response
 
 next_message = raw_response.fetch.("choices").at(0).fetch("message")
+
+list_of_messages.push(next_message)
 
 pp next_message
